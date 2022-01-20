0145295fd1f05d1c90a698025d558d4b40886449
deskwebcode-enterprise <96129916+deskwebcode-enterprise@users.noreply.github.com>
2022-01-20 18:43:14 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,6 +3,11 @@ window.onload = () => {
     let btnHamburger = document.querySelector("#btn-hamburger");
     let btnHamburgerCount = 0;
 
+    let mainNav = document.querySelector("#main-nav");
+    let nameLogo = document.querySelector("#name-logo");
+    let mainA = document.querySelectorAll(".main-a");
+    let mainAactivated = document.querySelector("#main-a-activated");
+
     let btnLeftSlider = document.querySelector("#btn-left-slider");
     let btnRightSlider = document.querySelector("#btn-right-slider");
     let txtGroup = document.querySelectorAll(".txt-group");
@@ -81,5 +86,42 @@ window.onload = () => {
         txtGroup[cvar].classList.add("txt-group-display-on");
 
         circlesCount = cvar;
+    }
+
+    window.onscroll = () => {
+        if (window.scrollY >= 50) {
+            altarnate("#ffffff", "#2d2e2e", "5px solid #ffffff", "5px solid #2d2e2e");
+        }
+
+        else {
+            altarnate("#2d2e2e", "#ffffff", "5px solid #2d2e2e", "5px solid #ffffff");
+        }
+    }
+
+    function altarnate(c1, c2, bc1, bc2) {
+        mainNav.style.background = c2;
+        nameLogo.style.color = c1;
+
+        btnHamburger.querySelectorAll("div")[0].style.background = c1;
+        btnHamburger.querySelectorAll("div")[1].style.background = c1;
+        btnHamburger.querySelectorAll("div")[2].style.background = c1;
+
+        if (window.innerWidth > 1299) {
+            for (let i = 0; i < mainA.length; i ++) {
+                mainA[i].style.color = c1;
+                mainA[i].style.borderTop = bc2;
+                
+                mainA[i].onmouseover = () => {
+                    mainA[i].style.borderTop = bc1;
+                }
+
+                mainA[i].onmouseout = () => {
+                    mainA[i].style.borderTop = bc2;
+                    mainAactivated.style.borderTop = bc1;
+                }
+            }
+
+            mainAactivated.style.borderTop = bc1;
+        }
     }
 };
